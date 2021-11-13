389d303b184ac395340f1b3b17d416f186479fd4
Camille Lions <lionscamille@gmail.com>
2021-11-13 10:34:07 +0100

    progression

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,5 +1,7 @@
 const express = require('express'); 
 const router = express.Router(); 
+const postCtrl = require('../controllers/post'); 
+const commentCtrl = require('../controllers/comment');
 
 // import auth middleware
 // import controller
@@ -7,20 +9,16 @@ const router = express.Router();
 // import sanitize middleware
 
 // créer un post
-router.post('/');
-
+router.post('/', postCtrl.createPost);
 // modifier un post
-router.put('/:id');
-
+router.put('/:id', postCtrl.updatePost);
 // supprimer un post
-router.delete('/:id');
+router.delete('/:id', postCtrl.deletePost);
 
 // récupérer un post spécifique
 router.get('/:id');
-
 // récupérer tous les posts
 router.get('/');
-
 
 
 // ajouter ou retirer un like 
@@ -29,12 +27,10 @@ router.post('/:id/like');
 
 
 // ajouter un commentaire 
-router.post('/:id/comment');
-
+router.post('/:id/comment', commentCtrl.addComment);
 //modifier un commentaire
-router.put('/comment/:id');
-
+router.put('/comment/:id', commentCtrl.updateComment);
 //supprimer un commentaire
-router.delete('/comment/:id');
+router.delete('/comment/:id', commentCtrl.deleteComment);
 
 module.exports = router; 
